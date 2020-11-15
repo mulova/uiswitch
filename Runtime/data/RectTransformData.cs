845da72420641059d345380e4665bb30dc9a0a99
mulova <mulova@gmail.com>
2020-11-15 10:14:48 +0900

    remove useless RectTransformData data

--- Runtime/data/RectTransformData.cs
+++ Runtime/data/RectTransformData.cs
@@ -10,11 +10,8 @@ namespace mulova.switcher
     public class RectTransformData : TransformData
     {
         public Vector2 anchoredPosition;
-        public Vector3 anchoredPosition3D;
         public Vector2 anchorMax;
         public Vector2 anchorMin;
-        public Vector2 offsetMax;
-        public Vector2 offsetMin;
         public Vector2 pivot;
         public Vector2 sizeDelta;
 
@@ -25,14 +22,11 @@ namespace mulova.switcher
         {
             base.ApplyTo(c);
             var r = c as RectTransform;
+            r.pivot = pivot;
             r.anchoredPosition = anchoredPosition;
-            r.anchoredPosition3D = anchoredPosition3D;
+            r.sizeDelta = sizeDelta;
             r.anchorMax = anchorMax;
             r.anchorMin = anchorMin;
-            r.offsetMax = offsetMax;
-            r.offsetMin = offsetMin;
-            r.pivot = pivot;
-            r.sizeDelta = sizeDelta;
 
         }
 
@@ -40,14 +34,11 @@ namespace mulova.switcher
         {
             base.Collect(c);
             var r = rect;
+            pivot = r.pivot;
             anchoredPosition = r.anchoredPosition;
-            anchoredPosition3D = r.anchoredPosition3D;
+            sizeDelta = r.sizeDelta;
             anchorMax = r.anchorMax;
             anchorMin = r.anchorMin;
-            offsetMax = r.offsetMax;
-            offsetMin = r.offsetMin;
-            pivot = r.pivot;
-            sizeDelta = r.sizeDelta;
         }
 
         public override bool Equals(object obj)
@@ -55,11 +46,8 @@ namespace mulova.switcher
             var that = (RectTransformData)obj;
             return base.Equals(obj)
              && this.anchoredPosition == that.anchoredPosition
-             && this.anchoredPosition3D == that.anchoredPosition3D
              && this.anchorMax == that.anchorMax
              && this.anchorMin == that.anchorMin
-             && this.offsetMax == that.offsetMax
-             && this.offsetMin == that.offsetMin
              && this.pivot == that.pivot
              && this.sizeDelta == that.sizeDelta;
         }
@@ -67,14 +55,11 @@ namespace mulova.switcher
         public override int GetHashCode()
         {
             var hash = base.GetHashCode();
+            hash = hash * 37 + pivot.GetHashCode();
             hash = hash * 37 + anchoredPosition.GetHashCode();
-            hash = hash * 37 + anchoredPosition3D.GetHashCode();
+            hash = hash * 37 + sizeDelta.GetHashCode();
             hash = hash * 37 + anchorMax.GetHashCode();
             hash = hash * 37 + anchorMin.GetHashCode();
-            hash = hash * 37 + offsetMax.GetHashCode();
-            hash = hash * 37 + offsetMin.GetHashCode();
-            hash = hash * 37 + pivot.GetHashCode();
-            hash = hash * 37 + sizeDelta.GetHashCode();
             return hash;
         }
 
@@ -83,11 +68,8 @@ namespace mulova.switcher
             var that = t as RectTransformData;
             return base.TransformEquals(that)
                 && this.anchoredPosition.ApproximatelyEquals(that.anchoredPosition)
-                && this.anchoredPosition3D.ApproximatelyEquals(that.anchoredPosition3D)
                 && this.anchorMax.ApproximatelyEquals(that.anchorMax)
                 && this.anchorMin.ApproximatelyEquals(that.anchorMin)
-                && this.offsetMax.ApproximatelyEquals(that.offsetMax)
-                && this.offsetMin.ApproximatelyEquals(that.offsetMin)
                 && this.pivot.ApproximatelyEquals(that.pivot)
                 && this.sizeDelta.ApproximatelyEquals(that.sizeDelta);
         }
