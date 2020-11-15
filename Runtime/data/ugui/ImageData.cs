17e175a51830df6ad3b0106a3fb6bc499ba08a0a
mulova <mulova@gmail.com>
2020-11-15 12:28:05 +0900

    Add ImageData info

--- Runtime/data/ugui/ImageData.cs
+++ Runtime/data/ugui/ImageData.cs
@@ -9,6 +9,11 @@ namespace mulova.switcher
     {
         public Sprite sprite;
         public Image.Type imageType;
+        public float fillAmount;
+        public bool fillCenter;
+        public bool fillClockwise;
+        public Image.FillMethod fillMethod;
+        public int fillOrigin;
         public bool useSpriteMesh;
         public bool preserveAspect;
         public bool maskable;
@@ -17,6 +22,11 @@ namespace mulova.switcher
         {
             i.sprite = sprite;
             i.type = imageType;
+            i.fillAmount = fillAmount;
+            i.fillCenter = fillCenter;
+            i.fillClockwise = fillClockwise;
+            i.fillMethod = fillMethod;
+            i.fillOrigin = fillOrigin;
             i.useSpriteMesh = useSpriteMesh;
             i.preserveAspect = preserveAspect;
             i.maskable = maskable;
@@ -26,6 +36,11 @@ namespace mulova.switcher
         {
             sprite = img.sprite;
             imageType = img.type;
+            fillAmount = img.fillAmount;
+            fillCenter = img.fillCenter;
+            fillClockwise = img.fillClockwise;
+            fillMethod = img.fillMethod;
+            fillOrigin = img.fillOrigin;
             useSpriteMesh = img.useSpriteMesh;
             preserveAspect = img.preserveAspect;
             maskable = img.maskable;
@@ -36,6 +51,11 @@ namespace mulova.switcher
             var that = (ImageData)obj;
             return this.sprite == that.sprite
                 && this.type == that.type
+                && this.fillAmount == that.fillAmount
+                && this.fillCenter == that.fillCenter
+                && this.fillClockwise == that.fillClockwise
+                && this.fillMethod == that.fillMethod
+                && this.fillOrigin == that.fillOrigin
                 && this.useSpriteMesh == that.useSpriteMesh
                 && this.preserveAspect == that.preserveAspect
                 && this.maskable == that.maskable;
@@ -45,6 +65,11 @@ namespace mulova.switcher
         {
             var hash = sprite.GetHashCode();
             hash = hash * 37 + type.GetHashCode();
+            hash = hash * 37 + fillAmount.GetHashCode();
+            hash = hash * 37 + fillCenter.GetHashCode();
+            hash = hash * 37 + fillClockwise.GetHashCode();
+            hash = hash * 37 + fillMethod.GetHashCode();
+            hash = hash * 37 + fillOrigin.GetHashCode();
             hash = hash * 37 + useSpriteMesh.GetHashCode();
             hash = hash * 37 + preserveAspect.GetHashCode();
             hash = hash * 37 + maskable.GetHashCode();
